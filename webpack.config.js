35a838361c85d390707a4191fcbc0f55565d53cf
Mark D'Arensbourg <mdarens@gmail.com>
2016-06-21 16:17:22 -0500

    make demo work

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require("webpack");
+
+module.exports = {
+	entry: "./demos/index.js",
+	output: {
+		path: "./demos",
+		filename: "build.js"
+	},
+	resolve: {
+		extensions: [
+			'',
+			'.js'
+		],
+		modulesDirectories: [
+			"node_modules"
+		],
+	},
+	module: {
+		loaders: []
+	}
+};
